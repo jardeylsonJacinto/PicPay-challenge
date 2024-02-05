dacfaf362b487fcc79df3a0ee6788b268c07406e
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2024-02-04 22:27:36 -0300

    fix: a bug fix

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,4 +3,6 @@ import UserController from '../controllers/UserController';
 
 const router = Router();
 router.get('/', UserController.index);
+router.post('/', UserController.store);
+
 export default router;
