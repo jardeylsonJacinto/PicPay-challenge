a65194ad3331476cefad1fa5eba87f012d957009
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2024-02-05 21:01:23 -0300

    feat: create custom error Missing-param-error

--- src/errors/missing-param-error.ts
+++ src/errors/missing-param-error.ts
@@ -1,6 +1,6 @@
 export class MissingParamError extends Error {
   constructor(paraName: string) {
-    super(`missing param: ${paraName}`);
-    this.name = 'MissingParamError';
+    super('MissingParamError');
+    this.name = `missing param: ${paraName}`;
   }
 }
