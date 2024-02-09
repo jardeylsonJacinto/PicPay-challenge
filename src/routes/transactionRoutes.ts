eac45b7471c87f74b4d91603f436d928842ce76b
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2024-02-09 11:34:20 -0300

    feat: add id in the transaction route

--- src/routes/transactionRoutes.ts
+++ src/routes/transactionRoutes.ts
@@ -3,6 +3,6 @@ import TransactionController from '../controllers/TransactionController';
 
 const router = Router();
 router.get('/', TransactionController.index);
-router.post('/', TransactionController.store);
+router.post('/:id', TransactionController.store);
 
 export default router;
