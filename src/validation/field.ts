ca5985a81cc245873305803ce92e978dad8b9312
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2024-02-09 11:28:43 -0300

    refactor: refactor archive fields

--- src/validation/field.ts
+++ src/validation/field.ts
@@ -1,19 +1,49 @@
-import { Request } from 'express';
+import { Request, Response } from 'express';
+import { MissingParamError } from '../errors/missing-param-error';
+import { badRequest } from '../helpers/http-helper';
 
-export const userField = (req: Request) => {
+export const userField = (req: Request, res: Response) => {
   const requiredFields = ['fullName', 'cpf', 'email', 'password', 'amount'];
   for (const field of requiredFields) {
+    if (field === 'amount') {
+      if (req.body[field] === undefined) {
+        return res.send(badRequest(new MissingParamError(field)));
+      }
+      break;
+    }
+
+    if (!req.body[field]) {
+      return res.send(badRequest(new MissingParamError(field)));
+    }
+  }
+};
+
+export const merchantField = (req: Request) => {
+  const requiredFields = ['fullName', 'cnpj', 'email', 'password', 'amount'];
+  for (const field of requiredFields) {
+    if (field === 'amount') {
+      if (req.body[field] === undefined) {
+        return field;
+      }
+      return false;
+    }
     if (!req.body[field]) {
       return field;
     }
   }
 };
 
-export const merchantField = (req: Request) => {
-  const requiredFields = ['fullName', 'cnpj', 'email', 'password'];
+export const transactionFieldValidation = (req: Request, res: Response) => {
+  const requiredFields = ['value', 'payerId', 'payeeId'];
   for (const field of requiredFields) {
+    if (field === 'value') {
+      if (req.body[field] === undefined) {
+        return res.send(badRequest(new MissingParamError(field)));
+      }
+      break;
+    }
     if (!req.body[field]) {
-      return field;
+      return res.send(badRequest(new MissingParamError(field)));
     }
   }
 };
