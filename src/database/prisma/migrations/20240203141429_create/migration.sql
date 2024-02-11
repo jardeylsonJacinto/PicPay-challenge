575fe883fb9532ee2c841ca403e41bb494f14784
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2024-02-11 12:25:56 -0300

    refactor:  database data

--- src/database/prisma/migrations/20240203141429_create/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Transaction" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "value" REAL NOT NULL,
-    "payer" TEXT NOT NULL,
-    "payee" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
